78290d118912381497d8ac0bdb6d1ad17bbe140c
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 00:39:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,10 +94,8 @@ end
  end
  
 def self.first_student_in_grade_10
-    s = self.first_X_students_in_grade_10(1)
-    s.map do |i|
-      self.new_from_db(i)
-    end[0]
+    
+    end
   end
   
   def self.all_students_in_grade_X(grade)
