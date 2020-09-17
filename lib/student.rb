b2ac746e4d38fb95e7ea6124683cf7dfcd1a1815
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 00:45:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,11 +94,7 @@ end
  end
  
 def self.first_student_in_grade_10
-  sql = <<-SQL
-   SELECT * FROM students WHERE grade = 10 ORDER BY name LIMIT 1
-   SQL
-    DB[:conn].execute(sql).collect do |row|
-  self.new_from_db(row)
+ new = 
   end
 end
   
