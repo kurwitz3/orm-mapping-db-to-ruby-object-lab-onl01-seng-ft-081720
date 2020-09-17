57d23deda04e80d87e0c2439513c536f0ae68147
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 00:37:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,13 +93,13 @@ end
   end
  end
  
- def self.first_student_in_grade_10 
-    new  = self.first_X_students_in_grade_10(1)
-    new .map do |i|
+def self.first_student_in_grade_10
+    s = self.first_X_students_in_grade_10(1)
+    s.map do |i|
       self.new_from_db(i)
-    end
-
-end
+    end[0]
+  end
+  
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
        SELECT * FROM students WHERE grade = ?
