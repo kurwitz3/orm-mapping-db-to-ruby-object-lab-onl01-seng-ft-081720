659b20a09a8398c575a9f554a2889d2512bf9e51
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 00:10:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,6 +98,8 @@ end
    SELECT students.name FROM students WHERE students.grade = 10 LIMIT 1 
    SQL
    
-   DB[:conn].execute(sql)
+   DB[:conn].execute(sql).map do |row|
+ 
+  self.new_from_db(row)
 end
 end
