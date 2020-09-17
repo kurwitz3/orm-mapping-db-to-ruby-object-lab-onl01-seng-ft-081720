2cd16b99ff67950193e0f0aab8283e9b9536bfc5
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 00:30:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,4 +100,11 @@ end
     end
 
 end
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+       SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade)
+  end
 end
